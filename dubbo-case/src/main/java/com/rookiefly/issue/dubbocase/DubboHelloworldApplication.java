6dad4d50e8700ef56dde29efd15898edcd4b431d
rookiefly <rookiefly@163.com>
2019-04-17 23:07:38 +0800

    dubbo sentinel

--- dubbo-case/src/main/java/com/rookiefly/issue/dubbocase/DubboHelloworldApplication.java
+++ dubbo-case/src/main/java/com/rookiefly/issue/dubbocase/DubboHelloworldApplication.java
@@ -3,6 +3,9 @@ package com.rookiefly.issue.dubbocase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * vm options -Dcsp.sentinel.api.port=8720 -Dcsp.sentinel.dashboard.server=localhost:8697 -Dproject.name=dubbo-provider-demo
+ */
 @SpringBootApplication
 public class DubboHelloworldApplication {
 
